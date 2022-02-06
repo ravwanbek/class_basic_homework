f9de9134c1a61ea7cac41b75cb0d5038cad40b42
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-02-06 20:07:06 +0900

    2 in 1

--- create_two_attribute_one_objects.py
+++ create_two_attribute_one_objects.py
@@ -2,6 +2,4 @@ from create_two_attribute import Person
 
 #create an object named "person" whose name is "Ali", age is "25"
 
-person=Person('Ali','25')
-print(person.name)
-print(person.age)
+person=Person('Ali',25)
